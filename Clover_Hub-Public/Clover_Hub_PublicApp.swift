c626a761cdf1d375dfb04080b938917868442eb9
prishaanand <60487925+prishaanand@users.noreply.github.com>
2022-09-04 19:19:10 -0700

    Initial Commit

---
+++ Clover_Hub-Public/Clover_Hub_PublicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clover_Hub_PublicApp.swift
+//  Clover_Hub-Public
+//
+//  Created by Prisha Anand on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Clover_Hub_PublicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
